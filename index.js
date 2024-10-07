135cc565a8202538109f0f488d0f59fb0ff6b1ae
hhm <hamhmin7@naver.com>
2024-10-07 14:43:51 +0900

    d

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// app.get('/', (req, res) => {
+//     res.send('Hello, Express!');
+// });
+// 정적 파일 제공
+app.use(express.static('public'));
+
+// 기본 라우트
 app.get('/', (req, res) => {
-    res.send('Hello, Express!');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(PORT, () => {
