1d8524a05377a0223944462aa49850fc3dac73a5
hhm <hamhmin7@naver.com>
2024-10-07 14:58:44 +0900

    d

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const PORT = process.env.PORT || 3000;
 // app.get('/', (req, res) => {
 //     res.send('Hello, Express!');
 // });
-// 정적 파일 제공
-app.use(express.static('public'));
+// public 폴더를 정적 파일 제공 폴더로 설정
+app.use(express.static(path.join(__dirname, 'public')));
 
-// 기본 라우트
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
